76f4fb14e03745911b62ecaebb9317def988b2d5
JaroB2002 <116017813+JaroB2002@users.noreply.github.com>
2023-10-13 21:04:28 +0200

    Feat: add jokes api & food api

--- src/App.js
+++ src/App.js
@@ -1,78 +1,82 @@
-/*export default class App{
-       constructor(){
-              //console.log("App is loaded");
-              this.getLocation();
-       }
-
-         getLocation(){
-                  navigator.geolocation.getCurrentPosition(this.showPosition, this.showError);
-                  this.showPosition.bind(this),
-                  this.showError;
-         }
-
-            showPosition(position){
-                    //console.log(this)
-                    console.log(position);
-                    let x = position.coords.latitude;
-                    let y = position.coords.longitude;
-                    //console.log(x,y);
-                    this.getWeather(x,y);
-            }
-
-            getWeather(x,y){
-            // url https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&current_weather=true&forecast_days=1
-            // fetch, then log result
-                fetch('https://api.open-meteo.com/v1/forecast?latitude='+x+'&longitude='+y+'&hourly=temperature_2m&current_weather=true&forecast_days=1')
-                .then(response => response.json())
-                .then((data) => {
-                    //console.log(data);
-                    document.querySelector("h2").innerHTML = data.current_weather.temperature + "°C";
-                })
-                .catch(error => console.log(error));
-            }
-
-            showError(error){
-                    console.log(error);
-            }
-}*/
-/* is day or night*/
-/* check google ad formats */
-
-
-
 export default class App {
     constructor() {
-      console.log("Hello from App.js");
-      this.getLocation();
+        console.log("Hello from App.js");
+        this.getLocation();
+        this.loadRandomFoodPhoto();
+
     }
+
     //Get the location of the user
-      getLocation() {
-          navigator.geolocation.getCurrentPosition(this.showPosition.bind(this));
-      }
+    getLocation() {
+        navigator.geolocation.getCurrentPosition(this.showPosition.bind(this));
+    }
+
     //Show the position of the user
-      showPosition(position) {
-          console.log(position);
-          //x - coordinate
-          let x = position.coords.latitude;
-          console.log(x);
-          //y - coordinate
-          let y = position.coords.longitude;
-          console.log(position.coords.longitude);
-  
-          this.getWeather(x, y);
-      }
-      //Get the weather from the API
-      getWeather(x, y) {
-          //url: https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&current_weather=true&forecast_days=1
-          //fetch, then log results
-          fetch("https://api.open-meteo.com/v1/forecast?latitude=" + x + "&longitude=" + y + "&hourly=temperature_2m&current_weather=true&forecast_days=1")
-              .then((response) => response.json())
-              .then(data => {
-                  console.log(data)
-                  //Get the temperature from the API
-                  let temp = data.current_weather.temperature;
-                  document.querySelector("h2").innerHTML = temp + "°C";
-              })
-              .catch(error => console.log(error));
-      }
-  }
+    showPosition(position) {
+        console.log(position);
+        //x - coordinate
+        let x = position.coords.latitude;
+        console.log(x);
+        //y - coordinate
+        let y = position.coords.longitude;
+        console.log(position.coords.longitude);
+
+        this.getWeather(x, y);
+    }
+    loadRandomFoodPhoto() {
+        const photoContainer = document.querySelector("#food-photo");
+        photoContainer.src = "https://source.unsplash.com/400x300/?food";
+        photoContainer.onload = () => {
+            photoContainer.style.maxWidth = "100%"; /* Voeg deze regel toe */
+            photoContainer.style.maxHeight = "100%"; /* Voeg deze regel toe */
+        };
+    }
+    // Toon de willekeurige foto op het scherm
+    displayPhoto(photoUrl) {
+        const imageElement = document.createElement("img");
+        imageElement.src = photoUrl;
+        imageElement.alt = "Random Food Photo";
+        const photoContainer = document.querySelector("#photo");
+        photoContainer.appendChild(imageElement);
+    }
+
+
+    //Get the weather from the API
+    getWeather(x, y) {
+        //url: https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&current_weather=true&forecast_days=1
+        //fetch, then log results
+        fetch("https://api.open-meteo.com/v1/forecast?latitude=" + x + "&longitude=" + y + "&hourly=temperature_2m&current_weather=true&forecast_days=1")
+            .then((response) => response.json())
+            .then(data => {
+                console.log(data)
+                //Get the temperature from the API
+                let temp = data.current_weather.temperature;
+                document.querySelector("#temperature").innerHTML = temp + "°C";
+
+                // Call the Dad Jokes API if the temperature is 15 degrees or higher
+                if (temp >= 15) {
+                    this.getDadJoke();
+                }
+            })
+            .catch(error => console.log(error));
+    }
+
+    // Get a random dad joke from the Dad Jokes API
+    getDadJoke() {
+        fetch('https://icanhazdadjoke.com/', {
+            headers: {
+                Accept: "application/json"
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            // Get the joke from the API
+            const joke = data.joke;
+
+            // Display the joke on the screen
+            document.querySelector("#joke").innerHTML = joke;
+        })
+        .catch(error => console.error(error));
+    }
+}
+
